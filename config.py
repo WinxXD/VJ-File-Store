c520927b3fbf2f740cf96d6cb923630e60dece2d
WinxXD <167777352+WinxXD@users.noreply.github.com>
2024-12-30 18:37:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "gastric-harrier-itsparadoxical-baa4b9ce.koyeb.app/")
+URL = environ.get("URL", "olympic-hildagard-insananityxd-8ade4003.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
